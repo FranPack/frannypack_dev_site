29a7df2150f358848bae45a137b71fc481a696d5
FrannyPack_ <fmrivera30@ymail.com>
2023-02-27 07:16:08 -0500

    fixed unused imports

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Twemoji from "react-twemoji";
-import Hero from "../img/undraw.svg";
 import { TypeAnimation } from "react-type-animation";
 import "./components.css";
 

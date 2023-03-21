3d1cadb9eb7b33a029ad2093293fd5b372fd0eca
FrannyPack_ <fmrivera30@ymail.com>
2023-03-20 21:54:26 -0400

    implemented mobile navigation sidebar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,31 +1,50 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import "./components.css";
-import { faBars } from "@fortawesome/free-solid-svg-icons"
+import { faBars, faClose } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 const Header = () => {
-  const [isNavExpanded, setIsNavExpanded] = useState(true)
+  const [isNavExpanded, setIsNavExpanded] = useState(true);
   return (
-      <div className="nav-bar">
-        <div className="nav-container">
-          <nav className="nav d-flex">
-            <div className="d-flex">
-              <Link className="nav-link" to="/projects">
-                Projects
+    <div className="nav-bar">
+      <div className="nav-container">
+        <nav className="nav d-flex">
+          <div className="d-flex">
+            <Link className="nav-link" to="/projects">
+              Projects
+            </Link>
+            <Link className="nav-link" to="/resume">
+              Resume
+            </Link>
+          </div>
+        </nav>
+        <nav className="mobile-nav">
+          <Link
+            className="hamburger nav-link"
+            onClick={() => {
+              setIsNavExpanded((prev) => !prev) ;
+            }}
+          >
+            {isNavExpanded ? <FontAwesomeIcon icon={faBars} className="hamburger-icon" /> : <FontAwesomeIcon icon={faClose} className="hamburger-icon" />}
+          </Link>
+          <ul className={`menu-nav${isNavExpanded ? '' : ' show-menu'}`}>
+            <li>
+              <Link className="nav-link" to="/projects" onClick={() => setIsNavExpanded(false)}>
+              Projects
               </Link>
-              <Link className="nav-link" to="/resume">
+            </li>
+            <li>
+              <Link className="nav-link" to="/resume" onClick={() => setIsNavExpanded(false)}>
                 Resume
               </Link>
-            </div>
-          </nav>
-          <nav className="mobile-nav">
-            <Link className="hamburger nav-link" onClick={() => {setIsNavExpanded(!isNavExpanded)}}><FontAwesomeIcon icon={faBars} className="hamburger-icon" /></Link>
-          </nav>
-          <Link className="nav-link logo" to="/">
-            <img src="/Port-Logo.gif" alt="Logo" />
-          </Link>
-        </div>
+            </li>
+          </ul>
+        </nav>
+        <Link className="nav-link logo" to="/">
+          <img src="/Port-Logo.gif" alt="Logo" />
+        </Link>
       </div>
+    </div>
   );
 };
 
